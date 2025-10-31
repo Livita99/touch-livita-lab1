d36d9e5cdefafccfbaeabd63c1a51ee7d42e14c7
Livita <150226387+Livita99@users.noreply.github.com>
2025-10-31 15:15:56 +0700

    Update greeting message in test.js

--- test/test.js
+++ test/test.js
@@ -3,7 +3,7 @@ import app from "../app.js";
 import { expect } from "chai";
 
 describe("GET /", () => {
-  it("should return Hello, GitHub Actions!", async () => {
+  it("should return Hello, CI/CD!", async () => {
     const res = await request(app).get("/");
     expect(res.status).to.equal(200);
     expect(res.text).to.equal("Hello, GitHub Actions!");
