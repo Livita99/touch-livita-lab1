ffc83348c0514148ae105f7ac304e846c0b981cf
Livita <150226387+Livita99@users.noreply.github.com>
2025-11-02 19:15:48 +0700

    Use process.env.PORT for Render deployment

--- app.js
+++ app.js
@@ -3,7 +3,12 @@ import express from "express";
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello, testing pull request!");
+  res.send("Hello from Render!");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
 
 export default app;
--- test/test.js
+++ test/test.js
@@ -3,9 +3,9 @@ import app from "../app.js";
 import { expect } from "chai";
 
 describe("GET /", () => {
-  it("should return Hello, testing pull request!", async () => {
+  it("should return Hello from Render!", async () => {
     const res = await request(app).get("/");
     expect(res.status).to.equal(200);
-    expect(res.text).to.equal("Hello, testing pull request!");
+    expect(res.text).to.equal("Hello from Render!");
   });
 });
